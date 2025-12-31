9836c90626db892d95ec667efa1e62d2440a68fe
alireza <theethanwardhere@gmail.com>
2025-12-31 17:22:34 +0330

    list copy

--- src/JCF/TheCollection.java
+++ src/JCF/TheCollection.java
@@ -15,21 +15,27 @@ public class TheCollection {
         ifYouOrdered.add("One");
         ifYouOrdered.add("Two");
         ifYouOrdered.add("Three");
-
         System.out.println(ifYouOrdered);
 
-        // =================================================================================
-        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 45, 6, 89, 7));
+        System.out.println("=================================================================================");
+        List<Integer> numbers = new ArrayList<>(List.of(2, 3, 6, 5, 9));
         Iterator<Integer> in = numbers.iterator();
-        in.forEachRemaining(System.out::println);
-
-        // =================================================================================
-
+        //in.forEachRemaining(System.out::println);
+        while (in.hasNext()) {
+            System.out.println("next : " + in.next());
+        }
+        System.out.println("=================================================================================");
         List<Integer> numbers2 = Arrays.asList(2, 3, 6, 5, 9);
         ListIterator<Integer> a = numbers2.listIterator(numbers2.size());
-        while (a.hasPrevious()){
+        while (a.hasPrevious()) {
             System.out.println("prev : " + a.previous());
         }
+        System.out.println("=================================================================================");
+        List<Integer> original = new ArrayList<>(List.of(1, 32, 54, 4, 23, 1));
+        System.out.println("ORIGINAL : " + original);
+        List<Integer> copy = List.copyOf(original);
+        System.out.println("COPY : " + copy);
+        System.out.println("=================================================================================");
 
     }
 
